e67f804e56008da000060670e95449bcc3bd1bf4
krgandhi <krgandhi@KRGANDHI1-W541>
2016-11-17 07:48:05 -0700

    Commit 20161117 001

---
+++ DAT201x/DAT201x/TestScript01.sql
@@ -0,0 +1,21 @@
+﻿USE [AdventureWorksLT2012]
+GO
+
+SELECT [ProductID],[ListPrice], [StandardCost], [ListPrice] - [StandardCost] AS Margin, Color
+FROM [SalesLT].[Product]
+
+
+SELECT [ProductID], Color, Size
+FROM [SalesLT].[Product]
+
+
+SELECT * FROM [SalesLT].[Customer]
+
+SELECT [Title],[FirstName],[MiddleName],[LastName],[Suffix]
+FROM [SalesLT].[Customer]
+
+SELECT ISNULL([Title],'') + [LastName], [Phone]
+FROM [SalesLT].[Customer]
+
+SELECT CAST([CustomerID] AS NVARCHAR(20))+':'+[CompanyName]
+FROM [SalesLT].[Customer]
---
+++ SQLServer2016/AlwaysON/PowerShell.sql
@@ -0,0 +1,2 @@
+﻿Get-Cluster|fl *Subnet*
+Get-ClusterResource AWAG2016_LSNR2016 | Get-ClusterParameter HostRecordTTL, RegisterAllProvidersIP
